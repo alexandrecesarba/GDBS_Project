5e3520093de51a9b8f1b7b14532e8331d27f0120
alexandrecesarba <alexandrecesarba@icloud.com>
2025-01-13 09:53:27 +0100

    Initial Commit

---
+++ GDBS Project/GDBS_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GDBS_ProjectApp.swift
+//  GDBS Project
+//
+//  Created by Alexandre César Brandão de Andrade on 13/01/25.
+//
+
+import SwiftUI
+
+@main
+struct GDBS_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GDBS ProjectTests/GDBS_ProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  GDBS_ProjectTests.swift
+//  GDBS ProjectTests
+//
+//  Created by Alexandre César Brandão de Andrade on 13/01/25.
+//
+
+import Testing
+@testable import GDBS_Project
+
+struct GDBS_ProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
